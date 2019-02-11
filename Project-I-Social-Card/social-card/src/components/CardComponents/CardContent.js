0645883903c0685ba37bf83dbad39ce91d92b9b3
Patrick <patrick.schwindt14@gmail.com>
2019-02-11 15:52:15 -0700

    Layout Complete

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent (){
+
+    return(
+        <div className ="cardContent">
+        <h3>Get Started with React</h3>
+        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.
+        </p>
+        </div>
+    );
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+function headerTitle(){
+    return <h1>Lambda School @Lambda School</h1>
+}
+
+export default headerTitle;
